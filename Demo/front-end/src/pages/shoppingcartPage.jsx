44edac078d6164d99f872ff8351b5b5e14dcee7c
Tan Do <tan.dothanh2506@hcmut.edu.vn>
2025-05-02 17:07:53 +0700

    resolved conflict

--- Demo/front-end/src/pages/shoppingcartPage.jsx
+++ Demo/front-end/src/pages/shoppingcartPage.jsx
@@ -7,7 +7,6 @@ function ShoppingCartPage() {
   const navigate = useNavigate();
   // Local state to store quantities per book (using book id as key)
   const [quantities, setQuantities] = useState({});
-  
 
   // State for checkout modal
   const [showCheckoutModal, setShowCheckoutModal] = useState(false);
